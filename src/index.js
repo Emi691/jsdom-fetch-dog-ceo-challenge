663e5cb249b03ebb1c4584e4deb210e8617d1a9b
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-19 15:25:33 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(resp => resp.json())
+.then(json => addImages(json))
+
+function addImages(json){
+    let array = json.message
+    for (let i = 0; i < array.length; i++) {
+        let pic = document.createElement("img")​
+        pic.setAttribute('src', array[i])
+        document.querySelector("#dog-image-container").appendChild(pic)
+    } 
+}
